421cfb70c9d5de354017eb50e32037a925902996
Ruumaa <titankalbuesa01@gmail.com>
2025-02-03 22:07:50 +0700

    feat: setup react-native expo and native wind

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text className="font-bold text-4xl font-rubik ">Welcome to ReState</Text>
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
